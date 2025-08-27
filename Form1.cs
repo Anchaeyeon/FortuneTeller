cfbbf435dda4e44f7f05a56d1e19a186e3348ff1
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-08-27 10:15:15 +0900

    #9 FormHistory 기능 추가 및 히스토리 로드 개선

--- Form1.cs
+++ Form1.cs
@@ -51,7 +51,7 @@ namespace FortuneTeller
             }
             else
             {
-                form = new FormHistory();
+                form = new FormHistory(this);
                 form.Show();
             }
         }
